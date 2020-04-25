439af7a7f07d709b32bbb90c84786551c6d37504
Mattia Lambertini <mattia.lambertini@protonmail.com>
2020-04-25 11:59:39 +0200

    Add dns hostname resolution

--- terraform/prod/network/main.tf
+++ terraform/prod/network/main.tf
@@ -12,6 +12,7 @@ module "vpc" {
   enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true
+  enable_dns_hostnames   = true
   enable_s3_endpoint     = true
 
   tags = {
