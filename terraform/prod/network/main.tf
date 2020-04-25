ddc65279c14159b0845d27212247716f5385fcd3
Mattia Lambertini <mattia.lambertini@protonmail.com>
2020-04-25 11:21:56 +0200

    add tags needed by eks on subnets

--- terraform/prod/network/main.tf
+++ terraform/prod/network/main.tf
@@ -17,5 +17,10 @@ module "vpc" {
   tags = {
     Environment = local.environment
   }
-
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
 }
