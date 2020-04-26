2e270fc86e2fd6d83ff5f8d3ed4cfe5e7ea7c71a
Mattia Lambertini <mattia.lambertini@protonmail.com>
2020-04-26 13:14:32 +0200

    add all providers for eks

--- terraform/prod/ekscluster/data.tf
+++ terraform/prod/ekscluster/data.tf
@@ -9,3 +9,11 @@ data "terraform_remote_state" "network" {
     encrypt        = true
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
--- terraform/prod/ekscluster/providers.tf
+++ terraform/prod/ekscluster/providers.tf
@@ -2,3 +2,27 @@ provider "aws" {
   region  = local.region
   version = "~> 2.59"
 }
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
